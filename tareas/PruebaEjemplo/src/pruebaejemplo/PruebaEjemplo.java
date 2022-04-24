a1772f6db955019976c5991eff4c052ed30c3548
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2022-04-24 17:11:41 -0500

    tarea del ejemplo prueba

--- tareas/PruebaEjemplo/src/pruebaejemplo/PruebaEjemplo.java
+++ tareas/PruebaEjemplo/src/pruebaejemplo/PruebaEjemplo.java
@@ -11,9 +11,9 @@ public class PruebaEjemplo {
     
     public static void main(String[] args) {
         
-        Cuentahabiente usuario1 = new Cuentahabiente("PUCHIS", 422938540, 50000);
-        Cuentahabiente usuario2 = new Cuentahabiente("topola", 122222222, 20000);
-        Cuentahabiente usuario3 = new Cuentahabiente("willy", 19191911, 30000);
+        Cuentahabiente usuario1 = new Cuentahabiente("Alejandra", 422938540, 50000);
+        Cuentahabiente usuario2 = new Cuentahabiente("Denilson ", 122222222, 20000);
+        Cuentahabiente usuario3 = new Cuentahabiente("GUILLERMO ", 19191911, 30000);
        
         System.out.println(usuario1);
         
@@ -36,9 +36,9 @@ public class PruebaEjemplo {
       Cuentahabiente[] lista =  new Cuentahabiente[5];
         
        
-      lista[0] =new Cuentahabiente("Elias", 34321111, 70000 );  
+      lista[0] =new Cuentahabiente("Juan", 34321111, 70000 );  
         
-      lista[1] =new Cuentahabiente("TOPO", 0201010101, 12000); 
+      lista[1] =new Cuentahabiente("Raul", 0201010101, 12000); 
       
       lista[2] =new Cuentahabiente("Sergio", 49678902, 45000);
       
