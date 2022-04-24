1a8e3755621ee47552c81dd9ef22617345166ba6
JaimeGuzmanSantoyo <jaimeguzman40@aragon.unam.mx>
2022-04-24 17:32:48 -0500

    tarea de entrega numero 2

--- tareas/PruebaEjemplo/src/pruebaejemplo/Cuentahabiente.java
+++ tareas/PruebaEjemplo/src/pruebaejemplo/Cuentahabiente.java
@@ -1,1 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaejemplo;
+
+/**
+ *
+ * @author josej
+ */
+public class Cuentahabiente {
+    
  
+    private String NombreCliente;
+    private int ide;
+    private int balance;
+
+    public Cuentahabiente() {
+    }
+
+    public Cuentahabiente(String NombreCliente, int ide, int balance) {
+        this.NombreCliente = NombreCliente;
+        this.ide = ide;
+        this.balance = balance;
+    }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String NombreCliente) {
+        this.NombreCliente = NombreCliente;
+    }
+
+    public int getIde() {
+        return ide;
+    }
+
+    public void setIde(int ide) {
+        this.ide = ide;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuentahabiente{" + "NombreCliente=" + NombreCliente + ", ide=" + ide + ", balance=" + balance + '}';
+    }
+ 
+    
+  public void retirarDinero( float monto ){
+          
+          System.out.println("el balance totoal es de : " );
+                 System.out.println(this.balance - 10000); ;
+          if(balance < 50000){
+              System.out.println("cliente regular y el nombre de la cuenta es  : " + this.NombreCliente);
+          }else{
+              System.out.println("cliente premium y el nombre de la cuenta es  "+ this.NombreCliente);
+              
+              
+          }
+  }
+}
+}
--- tareas/PruebaEjemplo/src/pruebaejemplo/PruebaEjemplo.java
+++ tareas/PruebaEjemplo/src/pruebaejemplo/PruebaEjemplo.java
@@ -1,10 +1,69 @@
+
+package pruebaejemplo;
+
+/**
+ *
+ * @author josej
+ */
+public class PruebaEjemplo {
+
+    
+    public static void main(String[] args) {
+          
+        Cuentahabiente usuario1 = new Cuentahabiente("Alejandra", 422938540, 50000);
+        Cuentahabiente usuario2 = new Cuentahabiente("Denilson ", 122222222, 20000);
+        Cuentahabiente usuario3 = new Cuentahabiente("GUILLERMO ", 19191911, 30000);
+       
+        System.out.println(usuario1);
+        
+      usuario1.retirarDinero(50000);
+      
+      
+      System.out.println(usuario2);
+
+      
+       usuario2.retirarDinero(20000);
+       
+       
+       System.out.println(usuario3);
+       
+       usuario3.retirarDinero(30000);
+    
+    
+       System.out.println("--------------LISTA DEL  ARREGLO---------- ");
+      
+      Cuentahabiente[] lista =  new Cuentahabiente[5];
+        
+       
+      lista[0] =new Cuentahabiente("Juana", 34321111, 70000 );  
+        
+      lista[1] =new Cuentahabiente("Raul", 0201010101, 12000); 
+      
+      lista[2] =new Cuentahabiente("Sergio", 49678902, 45000);
+      
+      lista[3] =new Cuentahabiente("Andrea", 49678902, 75000);
+      
+      lista[4] =new Cuentahabiente("Marcela ", 49678902, 85000);
+       //System.out.println("datos de los usuarios ");
+       
+     
+   
+        for (Cuentahabiente cuentahabiente : lista) {
+            cuentahabiente.retirarDinero(70000);
+             
  
-  
+          
+            
+        }
+            
+        
        
       
-      
-  
-      
-  
+        }
+    
+    }
+    
+    
 
+    
 
